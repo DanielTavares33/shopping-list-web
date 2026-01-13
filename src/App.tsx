c949f1fdc125051246f0b2e68db2405ebfd51e7f
Daniel Tavares <danieltvs33@gmail.com>
2026-01-13 20:48:38 +0000

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
     <>
-      
+      <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+        <h1 className="mb-4 text-3xl font-bold">Shopping List App</h1>
+        <Button variant="default">Add Item</Button>
+      </div>
     </>
   )
 }
